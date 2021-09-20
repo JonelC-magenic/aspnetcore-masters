e224ebe4b70288db11921b94dff2d1fae170d650
CatapangJay <38466188+CatapangJay@users.noreply.github.com>
2021-09-20 20:00:20 +0800

    - session 2 homework

---
+++ src/ASPNetCoreMastersTodoList.Api/Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMastersTodoList.Api.Controllers
+{
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemService _itemService;
+
+        public ItemsController(ItemService itemService)
+        {
+            _itemService = itemService ?? throw new ArgumentNullException(nameof(itemService));
+        }
+
+        public ActionResult<IEnumerable<string>> Get(int id)
+        {
+            return Ok(_itemService.GetAll(id));
+        }
+    }
+}
